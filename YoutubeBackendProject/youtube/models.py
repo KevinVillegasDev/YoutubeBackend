7eb20ef121d110728fe24f37688cfbd4dc7e1d43
KevinVillegasDev <kevin.villegas94@gmail.com>
2021-08-18 14:03:37 -0700

    Changed props in models

--- YoutubeBackendProject/youtube/models.py
+++ YoutubeBackendProject/youtube/models.py
@@ -7,7 +7,7 @@ class Comment(models.Model):
     comments = models.CharField(max_length=1500)
     likes = models.PositiveIntegerField(default=0)
     dislikes = models.PositiveIntegerField(default=0)
-    replies = models.CharField(max_length=1500, null=True)
+    replies = models.CharField(max_length=1500, blank=True, null=True)
     video = models.CharField(max_length=150, default='Default Value')
     
     
