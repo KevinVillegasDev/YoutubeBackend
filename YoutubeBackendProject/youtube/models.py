694cb1df5daeae7c306f40af50d3301aefc1b4ee
KevinVillegasDev <kevin.villegas94@gmail.com>
2021-08-24 14:24:05 -0700

    new model migrations

--- YoutubeBackendProject/youtube/models.py
+++ YoutubeBackendProject/youtube/models.py
@@ -5,8 +5,8 @@ from django.db import models
 
 class Comment(models.Model):
     comments = models.CharField(max_length=1500)
-    likes = models.PositiveIntegerField(default=0)
-    dislikes = models.PositiveIntegerField(default=0)
+    likes = models.PositiveIntegerField(default=0, blank=True, null=True)
+    dislikes = models.PositiveIntegerField(default=0, blank=True, null=True)
     replies = models.CharField(max_length=1500, blank=True, null=True)
     video = models.CharField(max_length=150, blank=True, null=True)
     
